da4e95813b11c5157d0a3aae8e7ca783341a4f61
farukahmed31b32 <farukahmed31b32@gmail.com>
2022-05-27 19:10:23 +0600

    Updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b763f7218b50ea01"; }
+/******/ 		__webpack_require__.h = function() { return "d5b26f1ea9108235"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
