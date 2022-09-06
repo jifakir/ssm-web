d78c24ef02c6c6f8981e05437f1ff306d4092ad1
Inioluwa Adebanjo <adeb6600@gmail.com>
2022-09-06 15:26:41 +0100

    add flicker to about us

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,6 +4,7 @@ import Button from '../components/UI/Button';
 import {url} from '../utils/flickr';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import FlickerCard from '../components/FlickerCard';
 
 const About = () => {
 
@@ -98,19 +99,10 @@ const About = () => {
             </div>
             <div className="w-[90%] mx-auto my-10 md:my-16">
                 <h3 className="text-2xl text-center lg:text-left">Connect with us on social media!</h3>
-                <div className="mt-3 pb-5 md:flex gap-5 md:gap-4.5 space-y-5 md:space-y-0">
-                    {
-                        filcker?.data?.photos.photo?.slice(0,5).map((photo, idx) => (
-                        <div className="relative w-full h-52 sm:h-72 md:w-60 md:h-32 lg:h-56" key={idx}>
-                            <Image 
-                                src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`} 
-                                alt="Pic" 
-                                layout='fill' 
-                                objectFit='cover' />
-                        </div>
-                        ))
-                    }
-                </div>
+                <div className="mb-16">
+        <FlickerCard />
+      </div>
+
             </div>
         </div>
     )
