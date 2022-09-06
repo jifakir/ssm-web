665553ad66b9328b0350a5000b3f4da7c77a3f66
Inioluwa Adebanjo <adeb6600@gmail.com>
2022-09-06 15:17:37 +0100

    add woxotech

--- components/FlickerCard.jsx
+++ components/FlickerCard.jsx
@@ -18,17 +18,21 @@ const FlickerCard = () => {
   if(!filcker){
     return <Loader className='text-5xl text-secondary' />
   }
-    return (
-        <div className="px-[5%] md:px-[10%] my-5 md:flex gap-5 md:gap-5 space-y-5 md:space-y-0">
-          {
-            filcker?.data?.photos.photo?.slice(0,5).map((photo, idx) => (
-              <div className="relative w-full h-52 sm:h-72 md:w-50 md:h-32 lg:h-52" key={idx}>
-                <Image src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`} alt="Pic" layout='fill' objectFit='cover' />
-              </div>
-            ))
-          }
-        </div>
-    )
+  return  (<div
+    loading="lazy"
+    data-mc-src="f9c6ff78-bf25-4935-b528-b14708b249a9#instagram"></div>)
+
+    // return (
+    //     <div className="px-[5%] md:px-[10%] my-5 md:flex gap-5 md:gap-5 space-y-5 md:space-y-0">
+    //       {
+    //         filcker?.data?.photos.photo?.slice(0,5).map((photo, idx) => (
+    //           <div className="relative w-full h-52 sm:h-72 md:w-50 md:h-32 lg:h-52" key={idx}>
+    //             <Image src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`} alt="Pic" layout='fill' objectFit='cover' />
+    //           </div>
+    //         ))
+    //       }
+    //     </div>
+    // )
 }
 
 export default FlickerCard;
