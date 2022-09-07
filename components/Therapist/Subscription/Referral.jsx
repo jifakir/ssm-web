23ed688c4b74d45ccd8b7fa64fcd99c542c99caa
Inioluwa Adebanjo <adeb6600@gmail.com>
2022-09-07 12:31:01 +0100

    display referral link

--- components/Therapist/Subscription/Referral.jsx
+++ components/Therapist/Subscription/Referral.jsx
@@ -43,7 +43,7 @@ const Referral = ({ profile }) => {
                         bg-white
                         cursor-pointer
                         rounded"
-                        value={"https://www.startsayingmore.com/" + data?.referral_link.slice(-7) || ''} 
+                        value={data?.referral_link} 
                         readOnly />
                 </div>
             </div>
